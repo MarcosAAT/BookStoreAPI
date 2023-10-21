76f537e13fc7d4af0f422697fa2e0bfed301cd20
Evon Troy Alexander <evont.alexander@gmail.com>
2023-10-21 15:12:15 -0400

    Reverted main branch to commit 271518f

--- src/main/java/com/getdata/restcall_test1/Repository/UserRepo.java
+++
@@ -1,7 +0,0 @@
-package com.getdata.restcall_test1.Repository;
-
-import com.getdata.restcall_test1.Entity.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, String>{
-}
